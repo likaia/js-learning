b598efaab19550f5e40e34c096ec7fd82e74cfb4
likaia <1195419506@qq.com>
2021-03-22 21:29:55 +0800

    feat: 添加对象的作用域示例

--- ClosuresAndScope/src/surface-application/ScopeApplication.js
+++ ClosuresAndScope/src/surface-application/ScopeApplication.js
@@ -1,16 +1,32 @@
 // 作用域提升
-{
-  function foo() {
-    console.log(1111);
+// {
+//   function foo() {
+//     console.log(1111);
+//   }
+//   foo(); // 2222
+//   foo = 1;
+//   // 报错：此时foo的值已经是1了，而并非一个函数
+//   // console.log(foo());
+//   function foo() {
+//     console.log(2222);
+//   }
+//   foo = 2;
+//   console.log(foo); // 2
+// }
+// console.log(foo); // 1
+
+var obj = {
+  name: "大白",
+  printName: function() {
+    console.log(name); // 从全局上下文中找name
   }
-  foo(); // 2222
-  foo = 1;
-  // 报错：此时foo的值已经是1了，而并非一个函数
-  // console.log(foo());
-  function foo() {
-    console.log(2222);
-  }
-  foo = 2;
-  console.log(foo); // 2
+};
+
+function getName() {
+  let name = "大白"; // 块级作用域，不会提升
+  return obj.printName;
 }
-console.log(foo); // 1
+let name = "神奇的程序员"; // 出现在全局上下文
+let printName = getName();
+printName(); // 神奇的程序员
+obj.printName(); // 神奇的程序员
