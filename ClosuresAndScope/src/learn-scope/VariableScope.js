c0735771252ab21b6e63d80ce65c7f89c320ba9b
likaia <1195419506@qq.com>
2021-03-29 21:39:37 +0800

    feat: 添加const在for循环与for..in循环对比的示例代码

--- ClosuresAndScope/src/learn-scope/VariableScope.js
+++ ClosuresAndScope/src/learn-scope/VariableScope.js
@@ -61,19 +61,37 @@
 // obj1.age = 20;
 // console.log(obj1.name);
 // console.log(obj1.age);
+//
+// var value = "global";
+//
+// // 例子1
+// (function() {
+//   console.log(value); // 报错：ReferenceError: Cannot access 'value' before initialization 临时死区(TDZ) 原因，let、const声明的变量会放到TDZ里，查找时会先从当前上下文里找从tdz里找到了value，所以它抛出啦错误
+//
+//   let value = "local";
+// })();
+//
+// // 例子2
+// {
+//   console.log(value); // 报错，原因同上
+//
+//   const value = "local";
+// }
 
-var value = "global";
+// var funcs = [],
+//   object = { a: 1, b: 1, c: 1 };
+// for (const key in object) {
+//   funcs.push(function() {
+//     console.log(key);
+//   });
+// }
+//
+// funcs[0](); // a 这是因为在 for in 循环中，每次迭代不会修改已有的绑定，而是会创建一个新的绑定。
 
-// 例子1
-(function() {
-  console.log(value); // 报错：ReferenceError: Cannot access 'value' before initialization 临时死区(TDZ) 原因，let、const声明的变量会放到TDZ里，查找时会先从当前上下文里找从tdz里找到了value，所以它抛出啦错误
-
-  let value = "local";
-})();
-
-// 例子2
-{
-  console.log(value); // 报错，原因同上
-
-  const value = "local";
+var funcs = [];
+for (const i = 0; i < 10; i++) {
+  funcs[i] = function() {
+    console.log(i);
+  };
 }
+funcs[0](); // 报错：Assignment to constant variable. 虽然每次都创建了一个新的变量，然而我们在迭代中(i++)尝试修改const声明的值
