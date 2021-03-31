464fb120d4e29885ee233b6173aae6a1bf3fc68b
likaia <1195419506@qq.com>
2021-03-31 09:24:13 +0800

    feat: 添加凡科笔试题

--- ClosuresAndScope/src/learn-scope/VariableScope.js
+++ ClosuresAndScope/src/learn-scope/VariableScope.js
@@ -96,11 +96,66 @@
 // }
 // funcs[0](); // 报错：Assignment to constant variable. 虽然每次都创建了一个新的变量，然而我们在迭代中(i++)尝试修改const声明的值
 
-var s2 = 10;
-function fn3() {
-  s2 += 10;
-  console.log(s2); // NaN 在函数上下文中找到了s2的声明，但是它是在未声明之前调用的，值为undefined，因此undefined+=10的值为NaN
-  const aa = "2";
-  var s2 = 20;
+// var s2 = 10;
+// function fn3() {
+//   s2 += 10;
+//   console.log(s2); // NaN 在函数上下文中找到了s2的声明，但是它是在未声明之前调用的，值为undefined，因此undefined+=10的值为NaN
+//   const aa = "2";
+//   var s2 = 20;
+// }
+// fn3();
+
+// let obj = {
+//   length: "凡科建站",
+//   exeFn: (function(length) {
+//     return function() {
+//       console.log(this.length);
+//       console.log(length);
+//     };
+//   })()
+// };
+//
+// const exeFn = obj.exeFn();
+// console.log(exeFn.length);
+
+/**
+ * eventLoop执行分析：
+ * 第1轮宏任务：
+     console.log("凡科建站") // 1 executor函数中的同步代码会立即执行
+     setTimeout加入宏任务队列
+     .then加入微任务队列
+     main（）函数声明
+     console.log(typeof main())  优先执行main函数，函数内的console.log("凡科营销") // 2 同步代码，立即执行
+     await Promise.all([p1, p2]) // 加入宏任务队列，阻塞其后面的同步代码
+     console.log(typeof main()) // 3 最后，执行type的值
+     开始读取微任务队列：
+     .then出队 // 4. 执行console.log("凡科公众号")
+
+   第2轮宏任务：
+   setTimeout从宏任务队列出队 // 5. 执行console.log("凡科商城");
+   await Promise.all([p1, p2]) 从宏任务出队 // 6. 执行console.log(await Promise.all([p1, p2]));
+   最后，mian函数内部的await执行完毕，阻塞结束，紧跟其后的同步的代码立即执行// 7. console.log("凡科自媒体");
+
+
+ * @type {Promise<unknown>}
+ */
+const p1 = new Promise(resolve => {
+  console.log("凡科建站"); // 1
+  setTimeout(() => {
+    resolve("凡科小程序");
+    console.log("凡科商城"); // 5
+  });
+});
+
+const p2 = Promise.resolve().then(() => {
+  console.log("凡科公众号"); // 4
+  return "凡科网";
+});
+
+async function main() {
+  console.log("凡科营销"); // 2
+  console.log(await Promise.all([p1, p2])); // 6
+  console.log("凡科自媒体"); // 7
 }
-fn3();
+
+console.log(typeof main()); // 3
